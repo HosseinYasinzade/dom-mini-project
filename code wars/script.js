96407b2631c1944ba0e0b864667eedb731dcfe1f
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-10-15 23:09:05 +0330

    add 2 codewars

---
+++ code wars/script.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+// const litres = (time) => Math.floor(time * 0.5);
+
+// ========================================================
+
+const invert = (array) => array.map((el) => el * -1);
